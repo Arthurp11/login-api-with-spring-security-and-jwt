21f2261a99bc84ce2acb70cfd6cb9d9b473ea940
Arthur <arthurp1190@gmail.com>
2025-07-30 03:25:24 -0300

    First commit

---
+++ src/main/java/com/example/login_api_with_spring_security_and_jwt/LoginApiWithSpringSecurityAndJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_api_with_spring_security_and_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiWithSpringSecurityAndJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiWithSpringSecurityAndJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_api_with_spring_security_and_jwt/LoginApiWithSpringSecurityAndJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_api_with_spring_security_and_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiWithSpringSecurityAndJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
